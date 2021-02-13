8a85c626aa4cfc532cc57ada9f54cc398513cdee
EhsanMi <79002196+EhsanMi@users.noreply.github.com>
2021-02-13 19:28:34 +0330

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anghezi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is ehsan");
+            Console.WriteLine("this is amir");
+        }
+    }
+}
